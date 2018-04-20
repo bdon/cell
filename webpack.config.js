6977ec3177ad4e82fbbcb2933f42a78a8cc48e58
Brandon Liu <bdon@bdon.org>
2018-04-20 16:36:56 -0400

    move pages to root

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: __dirname
   },
   mode:'development'
 };
